b336d3a2457f4b29b50aa280960ee3e4fb875a52
SunehaGumber <sunehagumber4@gmail.com>
2026-04-03 14:56:56 +0530

    fix: update CORS for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http:mern-interview-app-5upc.vercel.app",
     credentials:true
 }));
 
