1174f64da478fc8bc2d20bfa842b979dd0d7b589
SunehaGumber <sunehagumber4@gmail.com>
2026-04-06 09:58:31 +0530

    cors added before cookie parser

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,13 +6,13 @@ import interviewRouter from './routes/interview.routes.js';
 const app = express();
 
 
-
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
     origin: "https://mern-interview-app.vercel.app",
     credentials:true
 }));
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
 app.use('/api/interview',interviewRouter)
